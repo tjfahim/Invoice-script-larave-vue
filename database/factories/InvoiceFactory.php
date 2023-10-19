6bbe5f51d91fb85f28e74be146994bf525de730f
Fahim Islam <tjfahim1997@gmail.com>
2023-10-19 12:57:32 +0600

    database table complete with seed

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'number'=> $this->faker->numberBetween(10,1000),
+            'customer_id'=>$this->faker->numberBetween(10,1000),
+            'date'=>$this->faker->date,
+            'due_date'=> $this->faker->date,
+            'reference'=>'REF-'.rand(10,500),
+            'terms_and_conditions'=>$this->faker->paragraph(2),
+            'sub_total'=> $this->faker->numberBetween(10,1000),
+            'discount'=>$this->faker->numberBetween(10,1000),
+            'total'=>$this->faker->numberBetween(10,1000),
+            
+        ];
+    }
+}
